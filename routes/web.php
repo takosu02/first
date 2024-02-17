bf84bb29fcc60a04854d277267078547d042f5cf
takosu02 <omokawa1022@gmail.com.net>
2024-02-17 21:48:30 +0900

    a

--- routes/web.php
+++ routes/web.php
@@ -18,9 +18,3 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/posts', [PostController::class, 'index']);
-
-Route::get('/', function() {
-    return view('posts.index');
-});
-
-Route::get('/', [PostController::class,'index']);
