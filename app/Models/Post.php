d41128d83a26735428105355bedde2e82f7b6a0c
takosu02 <omokawa1022@gmail.com.net>
2024-02-22 03:36:16 +0900

    ブログ投稿

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -12,4 +12,9 @@ class Post extends Model
     public function getPaginateByLimit(int $limit_count = 10){
         return $this->orderBy('updated_at','DESC')->Paginate($limit_count);
     }
+    
+    protected $fillable = [
+        'title',
+        'body',
+    ];
 }
